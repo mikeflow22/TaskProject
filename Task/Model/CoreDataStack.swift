db1d379fde8a99d69bdc431c50ca8e4547242375
Michael R.F <mrflowers22@uwalumni.com>
2019-09-25 14:35:11 -0600

    Implemented coreDataSTack

--- Task/Model/CoreDataStack.swift
+++ Task/Model/CoreDataStack.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import CoreData
+//step 1
+import CoreData
+
+class CoreDataStack {
+    //step2
+    
+    //step 3: create a container to hold the stack
+    static let container: NSPersistentContainer = {
+        //create this container to be run later, when someone asks for it instead of immediately
+        let container = NSPersistentContainer(name: "Task")
+        //step 4 tell the container what to do
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    //step 5 create the context (MOC)
+  static var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+}
